9cf33c38ce8a2c59fdbc873982a705e0a0d188b1
Tamila <Tamila>
2023-05-24 12:19:13 +0300

    Skripts

---
+++ Assets/Skripti/DragNDrop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragNDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    //uzglabas noradi uz Objekti skriptu
+    public Objekti objektuSkripts;
+    //Uzglabas noradi uz katra objekta CanvaGroup
+    private CanvasGroup kanvasGrupa;
+    private RectTransform velkObjRectTrans;
+    public void OnBeginDrag(PointerEventData eventData) {
+       
+    }
+
+    public void OnDrag(PointerEventData eventData)  {
+       
+    }
+
+    public void OnEndDrag(PointerEventData eventData)  {
+
+    }
+
+
+    void Start () {
+		kanvasGrupa = GetComponent<CanvasGroup>();
+        velkObjRectTrans = GetComponent<RectTransform>();
+	}
+	
+	
+	
+}
